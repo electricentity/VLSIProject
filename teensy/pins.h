91f8ee6d968fdf0691b15e5ba31eefdf5c9999b7
Jacob Nguyen <jnguyen@hmc.edu>
2017-01-31 20:46:26 -0800

    teensy code

---
+++ teensy/pins.h
@@ -0,0 +1,67 @@
+#define P1 1 // Unused
+#define P2 2 // Unused
+
+//---Inputs---//
+#define P3 3 // reset
+//---Inputs---//
+
+//---Input/Output---//
+#define P4 4 // gnd
+#define P5 5 // vdd
+//---Input/Output---//
+
+//---Inputs---//
+#define P6 6 // col<3>
+#define P7 7 // col<2>
+#define P8 8 // col<1>
+#define P9 9 // col<0>
+
+#define P10 10 // row<3>
+#define P11 11 // row<2>
+#define P12 12 // row<1>
+#define P13 13 // row<0>
+
+#define P14 14 // direction
+#define P15 15 // player
+#define P16 16 // read
+
+//---MAY BE UNUSED---//
+#define P17 17 // ph1
+#define P18 18 // ph2
+//---MAY BE UNUSED---//
+
+//---Inputs---//
+
+//---Outputs---//
+#define P19 19 // data_ready
+
+#define P20 20 // data_out<0>
+#define P21 21 // data_out<1>
+#define P22 22 // data_out<2>
+#define P23 23 // data_out<3>
+#define P24 24 // data_out<4>
+#define P25 25 // data_out<5>
+#define P26 26 // data_out<6>
+#define P27 27 // data_out<7>
+#define P28 28 // data_out<8>
+#define P29 29 // data_out<9>
+#define P30 30 // data_out<10>
+#define P31 31 // data_out<11>
+//---Outputs---//
+
+#define P32 32 // Unused
+#define P33 33 // Unused
+#define P34 34 // Unused
+#define P35 35 // Unused
+
+//---Input/Output---//
+#define P36 36 // gnd
+#define P37 37 // vdd
+//---Input/Output---//
+
+#define P38 38 // Unused
+
+//---Input/Output---//
+#define P39 39 // vdd
+#define P40 40 // gnd
+//---Input/Output---//
