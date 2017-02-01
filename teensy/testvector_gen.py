9b26b6be6f6565bb654a9cc634968e4a8e89e947
Jacob Nguyen <jnguyen@hmc.edu>
2017-02-01 11:18:48 -0800

    Made it look nicer, has comments

--- teensy/testvector_gen.py
+++ teensy/testvector_gen.py
@@ -1,9 +1,25 @@
 f = open('testvector.txt', 'r')
 testvectors = [x.strip() for x in f.readlines()]
+
+"""
+data_out<23:0>
+inputs<15:0> = {4b\'0000, data_out<23:12>}
+outputs<15:0> = {4b\'0000, data_out<11:0>}
+"""
+
 inputs = [s[0:11] for s in testvectors]
 in_len = len(inputs)
 outputs = [s[12:23] for s in testvectors]
 out_len = len(outputs)
+
+
+print '/*'
+print 'data_out<23:0>'
+print 'inputs<15:0> = {4b\'0000, data_out<23:12>'
+print 'outputs<15:0> = {4b\'0000, data_out<11:0>'
+print '*/'
+
+print '\n'
 
 newline = 0;
 print "const uint16_t PROGMEM input_vals[] = {"
